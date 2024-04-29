85aa45641ffc791447c3189aba47929a4c9859f6
smith-steve <Steve.A.Dore@outlook.com>
2024-04-29 15:13:23 -0700

    added get search. We can search on text.

--- obj/Debug/net8.0/DotNetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotNetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21a6a6ba1b4bdf1771be27ea8af90ffa3059ec8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f067fed6a58a542444598d221618296885236eb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
